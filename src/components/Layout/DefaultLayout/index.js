24ae2b4f06d7639b2374a74421431693f4360b58
j4s0ntr <ngtrung4520@gmail.com>
2022-09-24 23:44:03 +0700

    DefaultLayout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,15 +1,20 @@
+import classNames from 'classnames/bind'
+import styles from './DefaultLayout.module.scss'
+//components
 import Header from '../components/Header/Header'
 import Sidebar from './Sidebar/Sidebar'
 
+const cx = classNames.bind(styles)
+
 const DefaultLayout = ({ children }) => {
     return (
-        <>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
-        </>
+        </div>
     )
 }
 
